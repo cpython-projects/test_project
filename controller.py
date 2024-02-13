007f8385ec6ce8c2b4c3e425aa5deaf47abfc5f9
OST <oleh.s.tymchuk@gmail.com>
2024-02-13 08:38:16 +0100

    initial commit

---
+++ controller.py
@@ -0,0 +1,18 @@
+def expenses_by_category(file_name: str) -> dict:
+    """
+    Returns a dictionary with the total expenses by category.
+
+    :param file_name: the name of the file to read
+    :return: a dictionary with the total expenses by category
+    """
+    ...
+
+
+def expenses_by_users(file_name: str) -> dict:
+    """
+    Returns a dictionary with the total expenses by user.
+
+    :param file_name: the name of the file to read
+    :return: a dictionary with the total expenses by user
+    """
+    ...
